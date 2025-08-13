76ef9b505f56fbb9ae5f294382ebfc3872d45bc6
Choudhury Debashis Dash <debashisdash1999@gmail.com>
2025-08-13 17:39:19 +0530

    Zepto project.sql

--- Zepto project.sql
+++ Zepto project.sql
@@ -1,14 +1,14 @@
 CREATE TABLE zepto(
-            sku_id SERIAL PRIMARY KEY,
-			category VARCHAR(120),
-			name VARCHAR(150) NOT NULL,
-			mrp NUMERIC(8,2),
-			discountPercent NUMERIC(5,2),
-			availableQuantity INTEGER,
-			discountedSellingPrice NUMERIC(8,2),
-			weightInGms INTEGER,
-			outOfStock BOOLEAN,
-			quantity INTEGER
+		            sku_id SERIAL PRIMARY KEY,
+					category VARCHAR(120),
+					name VARCHAR(150) NOT NULL,
+					mrp NUMERIC(8,2),
+					discountPercent NUMERIC(5,2),
+					availableQuantity INTEGER,
+					discountedSellingPrice NUMERIC(8,2),
+					weightInGms INTEGER,
+					outOfStock BOOLEAN,
+					quantity INTEGER
 );
 
 -- Data exploreation
@@ -122,4 +122,5 @@ SELECT
 FROM zepto
 WHERE outOfStock = FALSE
 GROUP BY category
+
 ORDER BY total_inventory_weight;
